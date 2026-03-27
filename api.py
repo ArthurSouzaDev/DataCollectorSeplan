7e30a1e0543a2294db8a137922fe68f21d2ce89b
Arthur Almeida de Souza <08423357147@tocantins.governo>
2026-03-27 13:45:43 -0300

    aplicando outros testes no conjunto de dados

--- api.py
+++ api.py
@@ -3,21 +3,32 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 fundo_a_fundo = os.getenv("URL_fundo_a_fundo")
+transf_especial = os.getenv("URL_trans_especial")
 
-params = {
-    # "ano_programa": "eq.2025"
-    'limit':50,
-    'uf_ente_recebedor_plano_acao':'eq.TO',
-    'data_inicio_vigencia_plano_acao':'gte.2026.01.01'  
+paramsFund = {
+    'limit':2,
+    'uf_ente_recebedor_plano_acao':'eq.TO', #Poderia ser adicionado um OR, porém não tem necessidade
+    # 'data_inicio_vigencia_plano_acao':'gte.2026.01.01', #opção de colocar ano
+    # 'situacao_plano_acao':'eq.EM_ELABORACAO'
+    # 'nome_ente_recebedor_plano_acao':'all.FME'eq.TO' or 
 }
 
-response = requests.get(fundo_a_fundo,params)
+
+
+paramsEspecial = {
+
+}
+
+response = requests.get(fundo_a_fundo,paramsFund)
+dados = response.json()
 count = 0
-if response.status_code == 200:
-    dados = response.json()
-    for item in dados:
-        count+=1
-        print(item)        
-else:   
-    print("Erro:", response.status_code)    
+
+# if response.status_code == 200:
+#     dados = response.json()
+#     for item in dados:
+#         count+=1
+#         print(item)        
+# else:   
+#     print("Erro:", response.status_code)    
 print(f"São {count} planos encontrados")
+print(dados)
