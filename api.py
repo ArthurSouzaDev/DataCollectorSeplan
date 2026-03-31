870431ec4fbaa50c868984fa0ba8c1d6fb0fffc1
Arthur Almeida de Souza <08423357147@tocantins.governo>
2026-03-31 08:58:22 -0300

    Aplicando alguns testes

--- api.py
+++ api.py
@@ -1,34 +1,24 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
 load_dotenv()
 fundo_a_fundo = os.getenv("URL_fundo_a_fundo")
 transf_especial = os.getenv("URL_trans_especial")
-
 paramsFund = {
-    'limit':2,
-    'uf_ente_recebedor_plano_acao':'eq.TO', #Poderia ser adicionado um OR, porém não tem necessidade
-    # 'data_inicio_vigencia_plano_acao':'gte.2026.01.01', #opção de colocar ano
-    # 'situacao_plano_acao':'eq.EM_ELABORACAO'
-    # 'nome_ente_recebedor_plano_acao':'all.FME'eq.TO' or 
-}
+    'uf_ente_recebedor_plano_acao':'eq.TO',
+
+    }
+
+def extract_data(endpoint,params):
+    response = requests.get(endpoint,params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Erro ao extrair dados da API: {response.status_code}")            
+        return None
+
+print(extract_data(fundo_a_fundo,paramsFund))
 
 
 
-paramsEspecial = {
-
-}
-
-response = requests.get(fundo_a_fundo,paramsFund)
-dados = response.json()
-count = 0
-
-# if response.status_code == 200:
-#     dados = response.json()
-#     for item in dados:
-#         count+=1
-#         print(item)        
-# else:   
-#     print("Erro:", response.status_code)    
-print(f"São {count} planos encontrados")
-print(dados)
